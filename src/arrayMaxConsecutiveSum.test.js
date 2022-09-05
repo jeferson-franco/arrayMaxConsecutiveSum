f5409fc9e11631375170a10b41ed856412dc605c
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-05 15:22:51 -0300

    test: create arrayMaxConsecutiveSum.test.js

---
+++ src/arrayMaxConsecutiveSum.test.js
@@ -0,0 +1,41 @@
+const solution = require('./arrayMaxConsecutiveSum.js');
+
+test('test 1', () => {
+    expect(solution([2, 3, 5, 1, 6], 2)).toBe(8);
+});
+
+test('test 2', () => {
+    expect(solution([2, 4, 10, 1], 2)).toBe(14);
+});
+
+test('test 3', () => {
+    expect(solution([1, 3, 2, 4], 3)).toBe(9);
+});
+
+test('test 4', () => {
+    expect(solution([3, 2, 1, 1], 1)).toBe(3);
+});
+
+test('test 5', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
+
+test('test 6', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
+
+test('test 7', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
+
+test('test 8', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
+
+test('test 9', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
+
+test('test 10', () => {
+    expect(solution([1, 3, 4, 2, 4, 2, 4], 4)).toBe(13);
+});
